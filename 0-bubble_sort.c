8b52f2180594a9d6a4999f0ad8666143af6fb36d
rotex <dantejake48@gmail.com>
2022-11-06 11:23:07 +0100

    change temp type to int

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,20 +10,21 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, temp, j;
+	size_t i, j;
+	int temp;
 
-	if (size < 2)
+	if (size < 2 || !array)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size; j++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[j] < array[j - 1])
 			{
 				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				array[j] = array[j - 1];
+				array[j - 1] = temp;
 				print_array(array, size);
 			}
 		}
