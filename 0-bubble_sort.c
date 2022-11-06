bce9d3b5d6ba606340ffda0b7cc7ee950f6832d8
Kaditcuy <philipukchimaobi@gmail.com>
2022-11-06 06:19:02 +0000

    Added function using bubble sort algorithim

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+* bubble_sort - sots an array of integers using bubble
+*				 sort algorithim
+* @array: Array of integers to print
+* @size: Size of Array
+*
+* Return : Nothing
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, temp, j;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < size; j++)
+		{
+			if (array[j] < array[j - 1])
+			{
+				temp = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
