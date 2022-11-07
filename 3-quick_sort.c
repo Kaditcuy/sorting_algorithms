0f18f4f1912941684f7a3cfde3734dd26231e440
Kaditcuy <philipukchimaobi@gmail.com>
2022-11-07 12:54:30 +0000

    Fixed documentation error

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,15 +1,25 @@
 #include "sort.h"
 
 /**
+* quick_sort - Sorts using quicksort algorithim
+* @array: Array of elements to sort
+* @size: size of array
 *
+* Return: NULL
 */
 void quick_sort(int *array, size_t size)
 {
-	lomuto_qsort(array, 0, size-1, size);
+	lomuto_qsort(array, 0, size - 1, size);
 }
 
 /**
+* lomuto_qsort - sorts using lomuto quick sort
+* @array: Array of elements to sort
+* @start: first index of array
+* @end: Last index of array
+* @size: size of array
 *
+* Return: NULL
 */
 void lomuto_qsort(int *array, int start, int end, size_t size)
 {
@@ -18,11 +28,19 @@ void lomuto_qsort(int *array, int start, int end, size_t size)
 	if (start < end)
 	{
 		pIndex = partition(array, start, end, size);
-		lomuto_qsort(array, start, pIndex-1, size);
-		lomuto_qsort(array, pIndex+1, end, size);
+		lomuto_qsort(array, start, pIndex - 1, size);
+		lomuto_qsort(array, pIndex + 1, end, size);
 	}
 }
-
+/**
+* partition - Divides an array of elements to sort
+* @array: Array of elements to sort
+* @start: First index of array
+* @end: last index of array
+* @size: size of array
+*
+* Return: partition index
+*/
 int partition(int *array, int start, int end, size_t size)
 {
 	int pivot = array[end];
@@ -35,20 +53,28 @@ int partition(int *array, int start, int end, size_t size)
 		{
 			swap(&array[i], &array[pIndex]);
 			pIndex++;
-			/*if (pIndex != i)
-			{
-				tmp = array[pIndex];
-				array[pIndex] = array[i];
-				array[i] = tmp;
-				print_array(array, size);
-			}*/
+			/*
+			* if (pIndex != i)
+			* {
+			*	tmp = array[pIndex];
+			*	array[pIndex] = array[i];
+			*	array[i] = tmp;
+			*	print_array(array, size);
+			* }
+			*/
 		}
 	}
 	swap(&array[pIndex], &array[end]);
 	print_array(array, size);
 	return (pIndex);
 }
-
+/**
+* swap - swaps two values
+* @a: index to swap
+* @b: index to swap
+*
+* Return: Null
+*/
 void swap(int *a, int *b)
 {
 	int temp;
