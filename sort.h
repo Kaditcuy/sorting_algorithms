62dcd1ccce28356b932b710aa5a1f60992f76085
Kaditcuy <philipukchimaobi@gmail.com>
2022-11-07 13:05:49 +0000

    Updated header file

--- sort.h
+++ sort.h
@@ -24,4 +24,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swap_nodes(listint_t **list, listint_t *nodeA, listint_t *nodeB);
+void lomuto_qsort(int *array, int start, int end, size_t size);
+int partition(int *array, int start, int end, size_t size);
 #endif
